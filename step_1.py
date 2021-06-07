e925aaa5372c6be6f8d47fa8105ebc0e5ce6db1e
EvgenDEP1 <jen_dostovalov@mail.ru>
2021-06-07 11:39:23 +0500

    step_1

--- step_1.py
+++ step_1.py
@@ -4,3 +4,10 @@ class Human:
         self.name = name
         self.date_birth = date_birth
         self.address = address
+
+
+class TrainingGroup:
+
+    def __init__(self, name, humans):
+        self.name = name
+        self.humans = humans
